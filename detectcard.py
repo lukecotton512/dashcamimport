55294f7300de6e522bd95d3148c22e3b6b602872
Luke Cotton <lukecotton@icloud.com>
2023-11-20 23:38:21 -0500

    Detects and mounts SD card.

--- detectcard.py
+++ detectcard.py
@@ -12,13 +12,14 @@ from pathlib import Path
 
 # Constants.
 SD_CARD_NAME = "NEXTBASE"
-SD_CARD_MOUNT_PATH = "/mnt/sdcardmnt"
+SD_CARD_MOUNT_PATH = "/mnt/dashcam"
 
 # Entry point for script.
 def main():
     # Setup response to signals.
     signal.signal(signal.SIGHUP, handleSignal)
     signal.signal(signal.SIGTERM, handleSignal)
+    signal.signal(signal.SIGINT, handleSignal)
 
     # Setup monitor to watch for dashcam SD card being mounted.
     context = pyudev.Context()
@@ -29,13 +30,14 @@ def main():
     for device in iter(monitor.poll, None):
         if device.action == 'add':
             devicelabel = device.get('ID_FS_LABEL', 'unknown')
-            print("{0}: {1}".format(device.device_node, devicelabel))
+            devicefstype = device.get('ID_FS_TYPE', '')
+            print(f"{device.device_node}: {devicelabel}")
             if devicelabel == SD_CARD_NAME:
-                mountSDCard(device.device_node)
+                mountSDCard(device.device_node, devicefstype)
 
 # Mount our SD card.
-def mountSDCard(devnode):
-    print("Mounting SD Card: {0}".format(devnode))
+def mountSDCard(devnode, fstype=''):
+    print(f"Mounting SD Card: {devnode}")
     
     # Check to see if mount point exists.
     mountPath = Path(SD_CARD_MOUNT_PATH)
@@ -49,11 +51,20 @@ def mountSDCard(devnode):
         os.mkdir(SD_CARD_MOUNT_PATH)
     
     # Mount the SD card.
-    os.system("mount {0} {1}".format(devnode, SD_CARD_MOUNT_PATH))
+    typearg = ""
+    if fstype != "":
+        typearg = f" -t {fstype}"
+
+    status = os.system(f"mount{typearg} {devnode} {SD_CARD_MOUNT_PATH}")
+
+    if status == 0:
+        print(f"Mounted {devnode} at {SD_CARD_MOUNT_PATH}")
+    else:
+        print(f"Failed to mount {devnode} at {SD_CARD_MOUNT_PATH}")
 
     
 # Handle a signal.
-def handleSignal():
+def handleSignal(sig, frame):
     print("Signal received")
     exit()
 
