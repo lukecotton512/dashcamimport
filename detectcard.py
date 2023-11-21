7e016bf89c28890898c639d6030eb82b2365ff86
Luke Cotton <lukecotton@icloud.com>
2023-11-21 18:05:59 -0500

    Card detect now runs import.

--- detectcard.py
+++ detectcard.py
@@ -8,7 +8,10 @@
 import pyudev
 import signal
 import os
+import sys
 from pathlib import Path
+
+import dashcamimport
 
 # Constants.
 SD_CARD_NAME = "NEXTBASE"
@@ -16,6 +19,13 @@ SD_CARD_MOUNT_PATH = "/mnt/dashcam"
 
 # Entry point for script.
 def main():
+    # Get path to import directory and exit if we don't have it.
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <import directory>")
+        exit(1)
+    
+    importDir = sys.argv[1]
+
     # Setup response to signals.
     signal.signal(signal.SIGHUP, handleSignal)
     signal.signal(signal.SIGTERM, handleSignal)
@@ -33,14 +43,19 @@ def main():
             devicefstype = device.get('ID_FS_TYPE', '')
             print(f"{device.device_node}: {devicelabel}")
             if devicelabel == SD_CARD_NAME:
-                mountSDCard(device.device_node, devicefstype)
+                mountStatus = mountSDCard(device.device_node, SD_CARD_MOUNT_PATH, devicefstype)
+                if mountStatus == 0:
+                    dashcamimport.doImport(SD_CARD_MOUNT_PATH, importDir)
+                    unmountSDCard(SD_CARD_MOUNT_PATH)
+                else:
+                    exit()
 
 # Mount our SD card.
-def mountSDCard(devnode, fstype=''):
+def mountSDCard(devnode, mountPoint, fstype=''):
     print(f"Mounting SD Card: {devnode}")
     
     # Check to see if mount point exists.
-    mountPath = Path(SD_CARD_MOUNT_PATH)
+    mountPath = Path(mountPoint)
     if mountPath.exists():
         # Check to see if we are not already mounted.
         if mountPath.is_mount():
@@ -48,19 +63,35 @@ def mountSDCard(devnode, fstype=''):
             return
     else:
         # Create the mount point.
-        os.mkdir(SD_CARD_MOUNT_PATH)
+        os.mkdir(mountPoint)
     
     # Mount the SD card.
     typearg = ""
     if fstype != "":
         typearg = f" -t {fstype}"
 
-    status = os.system(f"mount{typearg} {devnode} {SD_CARD_MOUNT_PATH}")
+    status = os.system(f"mount{typearg} {devnode} {mountPoint}")
 
     if status == 0:
-        print(f"Mounted {devnode} at {SD_CARD_MOUNT_PATH}")
+        print(f"Mounted {devnode} at {mountPoint}")
     else:
-        print(f"Failed to mount {devnode} at {SD_CARD_MOUNT_PATH}")
+        print(f"Failed to mount {devnode} at {mountPoint}")
+
+    return status
+
+# Unmount our SD card.
+def unmountSDCard(mountPath):
+    # Check to see if the card is mounted
+    mountPath = Path(mountPath)
+    if mountPath.exists() and mountPath.is_mount():
+        # Unmount the card.
+        status = os.system(f"umount {mountPath}")
+
+        # Check to make sure we were successful or not and print a corresponding message.
+        if status == 0:
+            print(f"Unmounted SD card {mountPath} successfully.")
+        else:
+            print(f"Failed to unmount SD card {mountPath}.")
 
     
 # Handle a signal.
